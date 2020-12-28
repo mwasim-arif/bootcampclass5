ecf0fdba4f34c642de8fb4f1d8c1813b1b4faafe
Muhammad Wasim Arif <76242160+mwasim-arif@users.noreply.github.com>
2020-12-28 15:13:05 +0500

    Update Few Variations

--- src/Child.js
+++ src/Child.js
@@ -7,7 +7,7 @@ const Child = () => {
 
     return (
         <div>
-            <h2>This first child using Counter Context</h2>
+            <h2>This First Child Using Counter Context</h2>
             <h4>Counter Value is: {counterValue[0]} </h4>
 
             <button onClick={()=> {counterValue[1](++counterValue[0])}}>Increment Contex</button>
